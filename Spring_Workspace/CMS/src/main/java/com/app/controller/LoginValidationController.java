02a77c7802d419d909d0e3ee453662d22ee6eaab
Tejas Kishor Kulkarni <tejas8102001@gmail.com>
2023-11-28 22:18:32 +0530

    User information is now sent with token

--- Spring_Workspace/CMS/src/main/java/com/app/controller/LoginValidationController.java
+++ Spring_Workspace/CMS/src/main/java/com/app/controller/LoginValidationController.java
@@ -1,5 +1,7 @@
 package com.app.controller;
 import com.dto.LoginResponse;
+import com.nimbusds.jwt.JWTClaimsSet;
+
 import java.util.Map;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +13,7 @@ import com.app.services.JwtTokenProvider;
 import com.app.services.LoginService;
 
 @RestController
-public class LoginValidationController {
+public class LoginValidationController<Claims> {
 
     private final LoginService loginService;
     private final JwtTokenProvider jwtTokenProvider;
@@ -23,15 +25,18 @@ public class LoginValidationController {
 
     @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/login")
-    public ResponseEntity<LoginResponse> loginUser(@RequestBody Map<String, String> loginData) {
+    public ResponseEntity<LoginResponse> loginUser(@RequestBody Map<String, String> loginData) throws Exception {
         String username = loginData.get("username");
         String password = loginData.get("password");
 
         if (username != null && password != null && loginService.checkLogin(username, password)) {
             String token = jwtTokenProvider.generateToken(username);
-            return ResponseEntity.ok(new LoginResponse(true, token));
+            JWTClaimsSet claimsSet = jwtTokenProvider.extractClaims(token);
+
+            return ResponseEntity.ok(new LoginResponse(true, token,claimsSet));
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse(false, null));
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new LoginResponse(false, null, null));
         }
     }
+    
 }
